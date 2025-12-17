a2389b355a15c4012e98f6f26eadca1fa7f002db
Nandani <bansalnandani620@gmail.com>
2025-12-17 14:31:40 +0530

    add button component with props interface and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 
 import './App.css'
-
+import Nav from "./components/nav.tsx";
 function App() {
 
 
   return (
     <>
+      <Nav/>
     <h1 style={{color:"red"}}>Hello React</h1>
+
     </>
+
   )
 }
 
